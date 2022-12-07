a0ef8539906aad15e0269a2c92a853a97d3e5df1
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-07 10:02:25 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = Math.sqrt(62);
+let num2 = Math.sqrt(62);
+
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random()*10) + 1 ;
+random;
+
+let num3 = 28;
+let num4 = 7;
+
+
+
+let mod ;
+mod = (num3 / num4) ;
+
+let max = Math.max(18,19,20);
